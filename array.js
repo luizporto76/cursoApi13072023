bf02ca9473bfd1e4c3a71d4d96f697e1c7368c6a
C@iu2019e <theo.ansi@gmail.com>
2023-07-13 09:05:28 -0300

    primeiro Commit

---
+++ array.js
@@ -0,0 +1,8 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const novoArray = array.map(item => item * 2)
+console.log(novoArray);   
+
+//const numerosParesFiltrados = array.filter(numero => numero % 2 === 0);
+//console.Log('Numero par: ',numerosParesFiltrados); 
+
---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyparser = require('body-parser')
+const usuarios = []; // Array de usuário
+
+app.use(bodyparser.urlencoded({ extended: true}))
+app.use(bodyparser.json())
+
+// inicio post 
+app.post('/usuarios', (req, res) => {
+    const usuario = req.body;
+    usuarios.push(usuario);
+    res.send(usuarios);
+});
+// Fim post 
+
+//Inicio Get 
+app.get('/usuariosUm', (req, res) => {
+    res.send(usuarios);
+});
+//Fim Get  
+
+//Inicio PUT 
+app.put('/usuariosPut/:id', (req, res) => {
+    const idex = usuarios.findIndex(user => user.id === ~~req.params.id); 
+    const usuario = req.body;
+    usuarios.splice(index, 1, usuario);
+    res.send(usuario); 
+});
+//Fim PUT 
+
+
+// inicio metodo GET 
+app.get('/usuarios/:id', (req, res) => {
+    const usuarios = [{
+            id: 1,
+            nome: 'João'
+        },
+        {
+            id: 2,
+            nome: 'Maria'
+        }
+    ]
+    const usuario = usuarios.find(user => ~~req.params.id === user.id);
+    res.send(usuarios)
+})
+// Fim metodo GET 
+
+app.get('/carros', (req, res) => {
+    res.send(
+      [{
+            id: 1,
+            nome: 'Ferrari'
+      },
+      {
+            id: 2, 
+            nome: 'Audio'
+      }
+  
+    ])
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
